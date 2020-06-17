2037a5033dd62dfff2cf78c35702b14549c03b4e
DBasias <dbasiadimas@hotmail.com>
2020-06-17 14:06:01 +0300

    added listByUser method in api-post to get a single user's posts in the view

--- client/post/api-post.js
+++ client/post/api-post.js
@@ -16,4 +16,21 @@ const listNewsFeed = async (params, credentials, signal) => {
   }
 };
 
-export { listNewsFeed };
+const listByUser = async (params, credentials) => {
+  try {
+    let response = await fetch("/api/posts/by" + params.userId, {
+      method: "GET",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+        Authorization: "Bearer " + credentials.t,
+      },
+    });
+
+    return await response.json();
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+export { listNewsFeed, listByUser };
